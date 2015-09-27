f237e06c10422e6b3e1e23e37839a6959fcf72b3
david allen <mail@ludimotion.com>
2015-09-27 03:11:06 -0400

    ofApp::draw() from ofxFaceTracker example-expression

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -26,7 +26,35 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofSetColor(255);
+    cam.draw(0, 0);
+    tracker.draw();
+    
+    int w = 100, h = 12;
+    ofPushStyle();
+    ofPushMatrix();
+    ofTranslate(5, 10);
+    int n = classifier.size();
+    int primary = classifier.getPrimaryExpression();
+    for(int i = 0; i < n; i++){
+        ofSetColor(i == primary ? ofColor::red : ofColor::black);
+        ofRect(0, 0, w * classifier.getProbability(i) + .5, h);
+        ofSetColor(255);
+        ofDrawBitmapString(classifier.getDescription(i), 5, 9);
+        ofTranslate(0, h + 5);
+    }
+    ofPopMatrix();
+    ofPopStyle();
+    
+    ofDrawBitmapString(ofToString((int) ofGetFrameRate()), ofGetWidth() - 20, ofGetHeight() - 10);
+    drawHighlightString(
+                        string() +
+                        "r - reset\n" +
+                        "e - add expression\n" +
+                        "a - add sample\n" +
+                        "s - save expressions\n"
+                        "l - load expressions",
+                        14, ofGetHeight() - 7 * 12);
 }
 
 //--------------------------------------------------------------
